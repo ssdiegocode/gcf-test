a7e9dc8aa36a42b971ec518ca4c0745559774c3c
ssdiegocode <167701206+ssdiegocode@users.noreply.github.com>
2025-03-02 09:08:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import functions_framework
 import requests
 from requests.structures import CaseInsensitiveDict
+import os
 
 @functions_framework.http
 def hello_http(request):
@@ -13,11 +14,11 @@ def hello_http(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
-    print("This script is running")
+    print("This script is running - function 2")
     url = os.getenv('SLACK_URL')
     headers = CaseInsensitiveDict()
     headers["Content-Type"] = "application/json"
 
-    data = '{"text":"Github code commited"}'
+    data = '{"text":"Github code commited - for function two"}'
     resp = requests.post(url, headers=headers, data=data)
     print(resp.status_code)
