887bdfbac80c5d6079c4a7ed77b8d553e00f2b86
ssdiegocode <167701206+ssdiegocode@users.noreply.github.com>
2025-03-02 14:02:23 -0300

    Update main.py

--- functions/function-slack-bot-3/main.py
+++ functions/function-slack-bot-3/main.py
@@ -1,1 +1,24 @@
-# teste
+import functions_framework
+import requests
+from requests.structures import CaseInsensitiveDict
+import os
+
+@functions_framework.http
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    print("___ This script is running - function 3")
+    url = os.getenv('SLACK_URL')
+    headers = CaseInsensitiveDict()
+    headers["Content-Type"] = "application/json"
+
+    data = '{"text":"___ Github code commited - for function three"}'
+    resp = requests.post(url, headers=headers, data=data)
+    print(resp.status_code)
