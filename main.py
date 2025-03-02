9cd97ade7735bf664ac37a1b92cda8898ddc7849
ssdiegocode <167701206+ssdiegocode@users.noreply.github.com>
2025-03-02 09:34:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ def hello_http(request):
     data = '{"text":"Github code commited - for function two"}'
     resp = requests.post(url, headers=headers, data=data)
     print(resp.status_code)
-    #update test
+    #update test - git version
