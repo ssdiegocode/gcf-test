a2f543e9a4a9b5e6ebc170aae2a1dcf7ab4c6e75
ssdiegocode <167701206+ssdiegocode@users.noreply.github.com>
2025-03-02 14:13:49 -0300

    Update variables.tf

--- terraform/function-slack-bot-3/variables.tf
+++ terraform/function-slack-bot-3/variables.tf
@@ -1,5 +1,11 @@
 variable "project_id" {}
+
 variable "region" {
   default = "us-east1"
 }
-variable "slack_url" {}
+
+variable "slack_url" {
+  description = "Slack webhook URL"
+  type        = string
+  default     = ""  # Allows local Terraform runs without needing this variable
+}
